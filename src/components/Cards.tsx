38aac03f0fcd15dac5db920662d30ad463894509
LuanaRodrigues1 <marialuana2810@gmail.com>
2024-06-24 21:56:07 -0300

    add tela dashboard

---
+++ src/components/Cards.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface CardsProps {
+  title: string;
+  description: string;
+  image: string;
+}
+
+export const Cards: React.FC<CardsProps> = ({ title, description, image }) => {
+  return (
+    <div className="border rounded-lg shadow-md bg-white overflow-hidden">
+      <div className="relative">
+        <img src={image} alt={title} className="h-64 w-full object-cover" />
+        <div className="absolute inset-x-0 bottom-0 h-px bg-gray-200"></div>
+      </div>
+      <div className="p-6">
+        <h3 className="text-xl font-semibold mb-2">{title}</h3>
+        <p className="text-gray-600">{description}</p>
+        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+          Preview
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/components/DashEditais.tsx
+++ src/components/DashEditais.tsx
@@ -1,31 +1,64 @@
-"use client"
-import React, { useState } from 'react';
-import { createUser } from '../lib/api';
-import {BiUser} from 'react-icons/bi';
-import {AiOutlineLock} from 'react-icons/ai';
-import Image from 'next/image';
-import { Header } from "./Header";
-import Link from 'next/link';
-import {MdOutlineEmail} from 'react-icons/md';
+"use client";
+import React, { useState } from "react";
+import { createUser } from "../lib/api";
+import { Cards } from "./Cards";
+import Sidebar from "./Sidebar";
 
 export function DashEditais() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     const response = await createUser(name, email, password);
     console.log(response);
-    if (response){
-        alert("Cadastro Realizado com sucesso!")
+    if (response) {
+      alert("Cadastro Realizado com sucesso!");
     } else {
-        alert("Conta já cadastrada")
+      alert("Conta já cadastrada");
     }
-  }
+  };
+
+  const cardData = [
+    {
+      title: "Is my community healthy?",
+      description:
+        "Id reprehenderit cupidatat duis proident ipsum cupidatat ea tempor non exercitation nulla duis sunt. Cupidatat labore irure...",
+      image: "/images/Marca-FACEPE.png",
+    },
+    {
+      title: "Most impactful channels",
+      description:
+        "Id reprehenderit cupidatat duis proident ipsum cupidatat ea tempor non exercitation nulla duis sunt. Cupidatat labore irure...",
+      image: "/images/Marca-FACEPE.png",
+    },
+  ];
 
   return (
-    <div className='relative h-screen '>
-    </div>
+    <>
+      <div className="min-h-screen">
+        <div className="flex items-center border-b border-gray-300">
+          <h1 className="text-3xl m-4 ">Dashboard</h1>
+        </div>
+        <div className="container flex">
+          <div className="w-1/4 h-screen">
+            <Sidebar userProfilePic="/images/user.jpeg" username="Jonh Due" />
+          </div>
+          <div className="w-3/4 p-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+              {cardData.map((card, index) => (
+                <Cards
+                  key={index}
+                  title={card.title}
+                  description={card.description}
+                  image={card.image}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
