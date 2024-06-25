8b0d4961557b3ffcc1c7db683a9befc5b2283015
Arthur Lima <arthurLM@protonmail.com>
2024-06-24 22:22:31 -0300

    import axios from axios;

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,52 +2,8 @@ import axios from "axios";
 
 const urlBase = "http://127.0.0.1:8000/api/"
 
-export async function createClient(id:number, nome: string, cpf: string, logradouro: string, numero: string, estado: string, cidade: string, cep: string) {
-  const response = await api.post('/cliente/create', {
-    id: `${id}`,
-    nome: nome,
-    cpf: cpf,
-    logradouro: logradouro,
-    numero: numero,
-    estado: estado,
-    cidade: cidade,
-    cep: cep,
-  });
-
-  return response;
-}
-
-export async function getClientInfo(id: string) {
-  const response = await api.get('/cliente/'+id, {});
-  return response.data;
-}
-
-export async function getClientList() {
-  try {
-    const response = await api.get('/clientes', {});
-    return response.data;
-  } 
-  catch (error) {
-    return false
-  }
-}
-
-export async function updateClient(id:number, nome: string, cpf: string, logradouro: string, numero: string, estado: string, cidade: string, cep: string) {
-  const response = await api.put('/cliente/update', {
-    id: `${id}`,
-    nome: nome,
-    cpf: cpf,
-    logradouro: logradouro,
-    numero: numero,
-    estado: estado,
-    cidade: cidade,
-    cep: cep,
-  });
-    return response;
-}
-
-export async function deleteClient(id: number) {
-  const response = await api.delete('/cliente/'+id, {});
+export async function getEditais() {
+  const response = await api.get('/editais', {});
   return response.data;
 }
 
