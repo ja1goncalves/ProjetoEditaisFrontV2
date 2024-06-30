5e97def5f0bd3c0c9c3ad7eba0e0e362914502ef
Arthur Lima <arthurLM@protonmail.com>
2024-06-30 15:24:10 -0300

    inicio tela inicial

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,36 +2,44 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { FaRegEye } from "react-icons/fa6";
+import { IoEnterOutline } from "react-icons/io5";
+
 
 interface TextProps {
-  url: string;
-  setUrl: (url: string) => void;
 }
 
 export function Header(props: TextProps) {
   return (
     <div 
-      className={`flex justify-center w-full bg-[#E9EDF0] py-3 bg-opacity-90 fixed top-0 z-40`} 
-      style={{
-        boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.2)',
-        transition: 'opacity 0.3s ease-in-out',
-      }}>
+      className={`flex justify-center w-full text-white font-semibold text-xl py-3 bg-opacity-90 absolute top-0 z-40`}>
       <div className="container h-full px-8 lg:px-0">
         <div className="flex items-center h-full justify-between">
-          <button onClick={() => props.setUrl("inicio")}>
-            LOGO
-          </button>
 
-         
-
-          <button
-            className="flex flex-row items-center gap-x-9 rounded-lg py-1 px-4 hover:opacity-60 hover:bg-gray-100"
-            style={{
-              boxShadow: 'inset -5px -5px 5px rgba(255, 255, 255, 0.3), inset 5px 5px 5px rgba(0, 0, 0, 0.3)',
-            }}
-          >
-            Usuário
+          <Link href="/">
+            <p className="flex flex-row items-center gap-x-2">EditalView <FaRegEye/></p>
+          </Link>
+          <ul className={`hidden lg:flex gap-x-6 items-center justify-center`}>
+            <li>
+              <Link href="/">
+                <p className={``}>Início</p>
+              </Link>
+            </li>
+            <li>
+              <Link href={``}>
+                <p className={``}>Editais Disponíveis</p>
+              </Link>
+            </li>
+            <li>
+              <Link href={``}>
+                <p className={``}>Sobre</p>
+              </Link>
+            </li>
+          </ul>
+          <button className="font-normal border border-white rounded-xl flex flex-row items-center gap-x-2 py-1 px-3 hover:opacity-60 hover:bg-gray-100">
+            <IoEnterOutline /> Login
           </button>
+          
         </div>
       </div>
     </div>
