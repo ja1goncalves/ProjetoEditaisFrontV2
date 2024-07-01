043345453dc4b4be69465aa086b0115b9657a7d6
LuanaRodrigues1 <marialuana2810@gmail.com>
2024-07-01 18:29:26 -0300

    Add tela de dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { DashAdm } from "@/components/DashAdm";
+import { DashBoard } from "@/components/DashBoard";
 import { DashEditais } from "@/components/DashEditais";
 
 export default function dashboard() {
-  return <DashAdm/>;
+  return <DashBoard />;
 }
--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -11,6 +11,7 @@ import igreja from "../../public/images/igreja.jpg";
 import portoGalinhas from "../../public/images/portoGalinhas.jpg";
 import recAnt1 from "../../public/images/recAntigo1.jpg";
 import recAntigo2 from "../../public/images/recAntigo2.png";
+import { FaSearch, FaFilter } from "react-icons/fa";
 
 import poli from "../../public/images/poli.png";
 import {
@@ -25,7 +26,7 @@ interface Card {
   edital: string;
 }
 
-export function DashEditais() {
+export function DashBoard() {
   const cardData = [
     {
       title: "20/2024 - IPECTI: Cidades inteligentes e resilientes",
@@ -100,7 +101,7 @@ export function DashEditais() {
   };
 
   const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = 16;
+  const totalPages = 3;
 
   const handlePageChange = (page: React.SetStateAction<number>) => {
     setCurrentPage(page);
@@ -125,30 +126,35 @@ export function DashEditais() {
 
   return (
     <>
-      <div className="min-h-screen">
+      <div className="min-h-screen bg-[#EBF4F6]">
         <HeaderOut />
         <div className="w-full mb-44">
           <section className="relative flex flex-col items-center min-h-[400px]">
-            <Image
-              src={images[currentIndex]}
-              alt="ImagemTuristica"
-              objectFit="fit"
-              className="h-[80vh] absolute opacity-40"
-            />
             <div className="w-full h-[80vh] bg-[#088395]" />
             <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center gap-y-7">
               <p className="text-white text-5xl font-medium px-96 text-center">
-                Encontre os editais ideais para você em um só lugar
+                Pesquisar
               </p>
               <p className="text-white font-[300] text-2xl px-96 text-center">
-                Acesse as principais oportunidades de apoio à pesquisa
-                oferecidas por instituições de referência
+                Descubra editais relevantes para você
               </p>
 
-              <button className="flex justify-center gap-x-3 leading-none border-none outline-none rounded-xl bg-[#088395] px-4 py-3 text-white font-semibold text-lg items-center cursor-pointer mb-6 hover:shadow-button-Home-hover-focus focus:shadow-button-Home-hover-focus">
-                <p>Comece agora a explorar os editais </p>
-                <IoIosArrowDown />
-              </button>
+              <div className="w-full h-full max-w-xl max-h-14 mt-4">
+                <div className="flex items-center overflow-hidden p-1 rounded-3xl">
+                  <input
+                    type="text"
+                    placeholder="Buscar editais..."
+                    className="text-gray-400 focus:ring-0 focus:border-1 focus:outline-none appearance-none leading-tight focus:border-white placeholder:text-generic-fields w-full border-none outline-none rounded-xl py-[1em]"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                  />
+
+                  <button>
+                    <FaMagnifyingGlass className="w-auto pl-4 text-[#37B7C3]" />
+                  </button>
+                </div>
+              </div>
             </div>
 
             <div className="absolute bottom-0 left-0 w-[100%] overflow-hidden">
@@ -167,96 +173,101 @@ export function DashEditais() {
             </div>
           </section>
         </div>
-        <div className="w-full h-full max-w-xl max-h-14 mt-4">
-          <div className="flex items-center overflow-hidden p-1 rounded-3xl">
-            <input
-              type="text"
-              placeholder="Buscar editais..."
-              className="text-gray-400 focus:ring-0 focus:border-1 focus:outline-none appearance-none leading-tight focus:border-white placeholder:text-generic-fields w-full border-none outline-none rounded-xl py-[1em]"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              onKeyDown={handleKeyDown}
-            />
 
-            <button>
-              <FaMagnifyingGlass className="w-auto pl-4 text-[#37B7C3]" />
-            </button>
-          </div>
-        </div>
-        <div className="flex flex-col border mx-40 rounded-3xl">
-          <div className="border-b flex flex-row w-full items-center justify-between px-24 py-4">
-            <p className="text-xl">
-              Total de {filteredCards.length} editais disponíveis
-            </p>
-            <div
-              className="bg-[#37B7C3] text-white flex items-center justify-around p-2 rounded-full"
-              style={{ width: "132px", height: "42px" }}
-            >
-              <button onClick={() => setVizualizacao("grid")} className="p-2">
-                <CiGrid41 size={24} />
-              </button>
-              <div className="border-l h-6 mx-2 border-white"></div>
-              <button onClick={() => setVizualizacao("row")} className="p-2">
-                <MdTableRows size={24} />
+        <div className="flex flex-col w-full">
+          <div className="flex items-center justify-between mb-8 mx-40">
+            <div className="flex items-center bg-white w-full rounded-2xl border">
+              <input
+                type="text"
+                placeholder="Buscar editais..."
+                className="text-gray-400 pr-2 focus:ring-0 focus:border-1 focus:outline-none appearance-none leading-tight focus:border-white placeholder:text-generic-fields w-full border-none outline-none rounded-xl py-[1em]"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyDown={handleKeyDown}
+              />
+              <button className="flex items-center justify-center bg-[#37B7C3] p-2 rounded-2xl px-12 py-3">
+                <FaSearch className="text-white" />
               </button>
             </div>
-          </div>
-          <div
-            className={`${
-              vizualizacao == "grid" ? "grid grid-cols-2" : "flex flex-col"
-            } m-10 gap-x-20 gap-y-16`}
-          >
-            {filteredCards.map((card, index) =>
-              vizualizacao === "grid" ? (
-                <CardsGrid
-                  key={index}
-                  title={card.title}
-                  publication={card.publication}
-                  edital={card.edital}
-                />
-              ) : vizualizacao === "row" ? (
-                <CardsRow
-                  key={index}
-                  title={card.title}
-                  publication={card.publication}
-                  edital={card.edital}
-                />
-              ) : null
-            )}
-          </div>
-          <div className="flex items-center justify-center border rounded-full p-2">
-            <button
-              onClick={handlePrevious}
-              disabled={currentPage === 1}
-              className="flex items-center text-[#37B7C3] disabled:opacity-50"
-            >
-              <FaChevronLeft />
-              <span className="ml-1">Anterior</span>
+            <button className="flex items-center justify-center bg-white p-2 rounded-2xl px-12 py-3">
+              <FaFilter className="text-[#37B7C3]" />
             </button>
-            <div className="flex mx-4">
-              {pageNumbers.map((number) => (
-                <button
-                  key={number}
-                  onClick={() => handlePageChange(number)}
-                  className={`mx-1 px-2 py-1 rounded-full ${
-                    currentPage === number ? "bg-[#37B7C3] text-white" : ""
-                  }`}
-                >
-                  {number}
+          </div>
+
+          <div className="flex flex-col border mx-40 rounded-3xl bg-white">
+            <div className="border-b flex flex-row w-full items-center justify-between px-24 py-4">
+              <p className="text-xl">
+                Total de {filteredCards.length} editais disponíveis
+              </p>
+              <div
+                className="bg-[#37B7C3] text-white flex items-center justify-around p-2 rounded-full"
+                style={{ width: "132px", height: "42px" }}
+              >
+                <button onClick={() => setVizualizacao("grid")} className="p-2">
+                  <CiGrid41 size={24} />
                 </button>
-              ))}
+                <div className="border-l h-6 mx-2 border-white"></div>
+                <button onClick={() => setVizualizacao("row")} className="p-2">
+                  <MdTableRows size={24} />
+                </button>
+              </div>
             </div>
-            <button
-              onClick={handleNext}
-              disabled={currentPage === totalPages}
-              className="flex items-center text-[#37B7C3] disabled:opacity-50"
+            <div
+              className={`${
+                vizualizacao == "grid" ? "grid grid-cols-2" : "flex flex-col"
+              } m-10 gap-x-20 gap-y-16`}
             >
-              <span className="mr-1">Próximo</span>
-              <FaChevronRight />
-            </button>
+              {filteredCards.map((card, index) =>
+                vizualizacao === "grid" ? (
+                  <CardsGrid
+                    key={index}
+                    title={card.title}
+                    publication={card.publication}
+                    edital={card.edital}
+                  />
+                ) : vizualizacao === "row" ? (
+                  <CardsRow
+                    key={index}
+                    title={card.title}
+                    publication={card.publication}
+                    edital={card.edital}
+                  />
+                ) : null
+              )}
+            </div>
+            <div className="flex items-center justify-center border rounded-full p-2">
+              <button
+                onClick={handlePrevious}
+                disabled={currentPage === 1}
+                className="flex items-center text-[#37B7C3] disabled:opacity-50"
+              >
+                <FaChevronLeft />
+                <span className="ml-1">Anterior</span>
+              </button>
+              <div className="flex mx-4">
+                {pageNumbers.map((number) => (
+                  <button
+                    key={number}
+                    onClick={() => handlePageChange(number)}
+                    className={`mx-1 px-2 py-1 rounded-full ${
+                      currentPage === number ? "bg-[#37B7C3] text-white" : ""
+                    }`}
+                  >
+                    {number}
+                  </button>
+                ))}
+              </div>
+              <button
+                onClick={handleNext}
+                disabled={currentPage === totalPages}
+                className="flex items-center text-[#37B7C3] disabled:opacity-50"
+              >
+                <span className="mr-1">Próximo</span>
+                <FaChevronRight />
+              </button>
+            </div>
           </div>
         </div>
-
         <div className="w-full mt-44">
           <section className="relative flex flex-col items-center min-h-[400px]">
             <div className="absolute top-0 left-0 w-[100%] overflow-hidden z-10">
