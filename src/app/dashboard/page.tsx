cbbba66abe6fe457de02ad99751a74d3ef41134f
LuanaRodrigues1 <marialuana2810@gmail.com>
2024-06-30 22:33:43 -0300

    Add funcionalidades da barra de pesquisa e seu design

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { DashAdm } from "@/components/DashAdm";
+import { DashEditais } from "@/components/DashEditais";
 
 export default function dashboard() {
-  return <DashAdm />;
+  return <DashEditais />;
 }
--- src/components/DashEditais.tsx
+++ src/components/DashEditais.tsx
@@ -3,27 +3,19 @@ import React, { useState } from "react";
 import { createUser } from "../lib/api";
 import { Cards } from "./Cards";
 import { Header } from "./Header";
-import Image from 'next/image'
+import Image from "next/image";
 
-import recAnt1 from '../../public/images/RecifeAntigo.png'
-import '../../public/curva.css'; 
+import recAnt1 from "../../public/images/RecifeAntigo.png";
+import lupa from "../../public/images/lupa.png";
+import "../../public/curva.css";
+
+interface Card {
+  title: string;
+  description: string;
+  image: string;
+}
 
 export function DashEditais() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    const response = await createUser(name, email, password);
-    console.log(response);
-    if (response) {
-      alert("Cadastro Realizado com sucesso!");
-    } else {
-      alert("Conta já cadastrada");
-    }
-  };
-
   const cardData = [
     {
       title: "Is my community healthy?",
@@ -39,22 +31,81 @@ export function DashEditais() {
     },
   ];
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [filteredCards, setFilteredCards] = useState<Card[]>(cardData);
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const response = await createUser(name, email, password);
+    console.log(response);
+    if (response) {
+      alert("Cadastro Realizado com sucesso!");
+    } else {
+      alert("Conta já cadastrada");
+    }
+  };
+
+  const searchCards = (searchTerm: string) => {
+    const lowerCaseSearch = searchTerm.toLowerCase().trim();
+
+    const filtered = cardData.filter((card) =>
+      card.title.toLowerCase().includes(lowerCaseSearch)
+    );
+
+    setFilteredCards(filtered);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      searchCards(searchTerm);
+    }
+  };
+
   return (
     <>
       <div className="min-h-screen">
-        <Header/>
+        <Header />
         <div className="w-full">
           <section className="relative flex flex-col items-center min-h-[400px]">
-            {/*<Image src={img01} alt="Pessoa utilizando a máquina da Recicletool" layout="fill" objectFit="contain" className="hidden lg:flex"/>*/}
-            <Image src={recAnt1} alt="ImagemTuristica" objectFit="contain" className="h-[80vh] absolute opacity-60"/>
-            <div className="w-full h-[80vh] bg-[#088395]"/>
-            <div className="curve"/>
+            <Image
+              src={recAnt1}
+              alt="ImagemTuristica"
+              objectFit="contain"
+              className="h-[80vh] absolute opacity-60"
+            />
+            <div className="w-full h-[80vh] bg-[#088395]" />
+            <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
+              <h1 className="text-white text-5xl font-medium mb-16 px-96 text-center">
+                Encontre os editais ideais para você em um só lugar
+              </h1>
+              <p className="text-white font-normal text-3xl mb-16 px-96 text-center">
+                Acesse as principais oportunidades de apoio à pesquisa
+                oferecidas por instituições de referência
+              </p>
+              <div className="w-full h-full max-w-xl max-h-14">
+                <div className="flex items-center overflow-hidden p-1 bg-white rounded-3xl">
+                  <Image src={lupa} alt={"Lupa"} className="w-14 pl-4" />
+                  <input
+                    type="text"
+                    placeholder="Buscar editais..."
+                    className="w-full px-4 py-2 text-gray-400 bg-white border-none"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                  />
+                </div>
+              </div>
+            </div>
           </section>
         </div>
         <div className="container flex">
           <div className="w-3/4 p-5">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-              {cardData.map((card, index) => (
+              {filteredCards.map((card, index) => (
                 <Cards
                   key={index}
                   title={card.title}
