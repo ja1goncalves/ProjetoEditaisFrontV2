0e5e56546250c6c44089eba7b8a6b9c2ca4191e7
Jean Elder Santana Araujo <jeanelder.sa@gmail.com>
2024-07-13 21:21:14 -0300

    Correções de erros do componente CardModel para build

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+// src/types.ts
+export interface Machine {
+    id: string;
+    name: string;
+    appkey: string;
+    addressTittle: string;
+    addressLabel: string;
+    addressMaps: string;
+    addressWaze: string;
+    latitude: number;
+    longitude: number;
+    shortLink: string;
+    ativa: boolean;
+  }
+  
