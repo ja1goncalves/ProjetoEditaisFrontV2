6e664f951a6957375823ed1025e45dc57c295ec2
Arthur Lima de Araujo Miranda <60785806+ArthurLimaMiranda@users.noreply.github.com>
2024-07-29 13:16:39 -0300

    maybe?

--- src/app/contexts/AuthContext.tsx
+++ src/app/contexts/AuthContext.tsx
@@ -43,11 +43,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }){
       const user: User = decode(token)
       setUser(user)
     }
-    const { '_vercel_jwt': tokenSafe } = parseCookies()
-    if(tokenSafe){
-      const user: User = decode(tokenSafe)
-      setUser(user)
-    }
   },[])
 
   async function signIn(data: signInData): Promise<void> {
@@ -64,9 +59,12 @@ export function AuthProvider({ children }: { children: React.ReactNode }){
                     return;
                 }
                 if (token) {
-                    setCookie(undefined, 'engsoft.token', token, {
-                        maxAge: 60 * 60 * 3, // 3 hours
-                    });
+                  setCookie(undefined, 'engsoft.token', token, {
+                    maxAge: 60 * 60 * 3,
+                    path: '/',
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: 'lax',
+                  });
                     router.push('/search');
                 }
             });
